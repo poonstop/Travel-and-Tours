f422a191afe3f25e027a55d518896a1ec287c420
Vect0 <151480608+Vect0@users.noreply.github.com>
2024-04-28 13:00:30 +0800

    added completed database schema
    ...i think its completed?

--- db_ttms.sql
+++ db_ttms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 26, 2024 at 02:57 PM
+-- Generation Time: Apr 28, 2024 at 06:58 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -69,6 +69,19 @@ CREATE TABLE `tbl_itinerary` (
   `optional` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `tbl_itinerary`
+--
+
+INSERT INTO `tbl_itinerary` (`itinerary_id`, `pack_code`, `day`, `meals`, `hotel_stat`, `hotel`, `activity`, `poi`, `optional`) VALUES
+(17, '', '1', 'X/L/X', 'Check-In', 'Golden Pacific Hotel  or similar', 'Visit', 'Chungshe Flower Garden > Sun Moon Lake > Tea Garden', 'Free time at Night Market'),
+(18, '', '2', 'B/L/X', 'Transfer', 'Tao Garden Hotel or similar', 'Visit', 'Cake Shop > Chiang Kai-Shek Shilin Residence Garden > Liberty Square > Taipei 101', 'Shopping at Ximending'),
+(19, '', '3', 'B/', 'Check-In', 'Tao Garden Hotel or similar', 'Visit', 'Jade Handicraft > Yehliu Geopark > Shifen waterfall > Shifen Old street', 'Chinese Lantern || Visit Duty free shop or cosmetic shop'),
+(44, '20240428_121402_738', '1', 'Hello', 'Check-In', 'Hello', 'Hello', 'Hello', 'Hello'),
+(45, '20240428_121402_738', '2', 'Hello', 'Check-In', 'Hello', 'Hello', 'Hello', 'Hello'),
+(46, '20240428_122944_121', '1', 'zvzv', 'N/A', 'zvzv', 'zv', 'vvzvz', 'vzv'),
+(47, '20240428_122944_121', '2', 'zvzv', 'N/A', 'zvzv', 'zvz', 'vzvz', 'vzvzv');
+
 -- --------------------------------------------------------
 
 --
@@ -76,12 +89,35 @@ CREATE TABLE `tbl_itinerary` (
 --
 
 CREATE TABLE `tbl_pack` (
-  `pack_code` int(11) NOT NULL,
+  `pack_code` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
   `locations` varchar(255) NOT NULL,
+  `inclusion` varchar(1000) NOT NULL,
+  `exclusion` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_pack`
+--
+
+INSERT INTO `tbl_pack` (`pack_code`, `title`, `locations`, `inclusion`, `exclusion`) VALUES
+('', 'adad', 'adad', 'adad', 'ad'),
+('1', 'ad', 'dad', 'sample list\n-hello\n-my\n-name\n-is\n-Ducky', 'adada'),
+('20240428_121402_738', 'Hello', 'Hello', 'Hello', 'Hello'),
+('20240428_122944_121', 'zvz', 'zvzv', 'zzvzv', 'zvzvz');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_price`
+--
+
+CREATE TABLE `tbl_price` (
+  `price_code` int(11) NOT NULL,
+  `pack_code` int(11) NOT NULL,
+  `currency` varchar(20) NOT NULL,
   `price` int(11) NOT NULL,
-  `inclusion` char(255) NOT NULL,
-  `exclusion` char(255) NOT NULL
+  `price_desc` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -130,6 +166,12 @@ ALTER TABLE `tbl_pack`
   ADD PRIMARY KEY (`pack_code`);
 
 --
+-- Indexes for table `tbl_price`
+--
+ALTER TABLE `tbl_price`
+  ADD PRIMARY KEY (`price_code`);
+
+--
 -- Indexes for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
@@ -155,13 +197,13 @@ ALTER TABLE `tbl_flight`
 -- AUTO_INCREMENT for table `tbl_itinerary`
 --
 ALTER TABLE `tbl_itinerary`
-  MODIFY `itinerary_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `itinerary_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
--- AUTO_INCREMENT for table `tbl_pack`
+-- AUTO_INCREMENT for table `tbl_price`
 --
-ALTER TABLE `tbl_pack`
-  MODIFY `pack_code` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tbl_price`
+  MODIFY `price_code` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
