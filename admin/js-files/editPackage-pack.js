1ba49268c81ce3f1234e3821a6753b454ac8b3df
Vect0 <151480608+Vect0@users.noreply.github.com>
2024-04-30 06:25:50 +0800

    few changes

--- admin/js-files/editPackage-pack.js
+++ admin/js-files/editPackage-pack.js
@@ -1,14 +1,9 @@
-/*
-data in the script
-pack_code(surrogate, PK)
-title(input text)
-locations(input text)
-price(input text)
-inclusions(input_text)
-exclusions(input_text)
-*/
+$(document).ready(function() {
+    //pull data from 
 
 
+
+});
 //edit package
 
 //delete package(optional)
