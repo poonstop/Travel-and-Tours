e92a4653eaff7c524d8a94c2e1f42f3512d6c9f6
Vect0 <151480608+Vect0@users.noreply.github.com>
2024-04-30 18:52:56 +0800

    JANNA EDIT MO ITOOOOOOO

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+    session_start();
+    if(isset($_SESSION['username'])){
+        if($_SESSION['userlvl']=='admin'){
+            header("location: Admin/admin.php");
+        }else{
+            header("location: index.php");
+        }
+    }
+
+    require("connection.php");
+    if(isset($_POST['uname'])){
+
+        $uname = $_POST['uname'];
+        $pass = $_POST['pass'];
+
+        $sql = "SELECT * FROM tbl_user WHERE username = '$uname' AND password = '$pass'";
+        //prepare
+        //bind
+        //execute
+        $query = $conn->query($sql);
+        if($query->rowCount() > 0){
+            foreach($query as $row){
+                $_SESSION['username'] = $row['username'];
+                $_SESSION['userlvl'] = $row['userlevel'];
+            }
+            if($_SESSION['userlvl']=='admin'){
+                echo "admin";
+            }else{
+                echo "client";
+            }
+
+        }else{
+            echo "error";
+        }
+    }
+?>
